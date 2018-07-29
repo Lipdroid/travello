2c50b07a16af832cc33edb0373f4de6ab7a85b16
lipuhossain <kutu1234>
2018-07-30 00:32:16 +0600

    added podfiles

---
+++ Pods/Target Support Files/Pods-Travello/Pods-Travello-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Travello : NSObject
+@end
+@implementation PodsDummy_Pods_Travello
+@end
--- Travello/Controllers/LoginVC.swift
+++ Travello/Controllers/LoginVC.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var btn_or: UIButton!
+    @IBOutlet weak var btn_fbLogin: FBSDKLoginButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,20 +23,26 @@ class LoginVC: UIViewController {
         if let version = Bundle.main.infoDictionary?["CFBundleVersion"]  as? String{
             //Check if version value set in userDefaults
             if let prev_version = UserDefaults.standard.object(forKey: Constants.VERSION) as? String{
-                if version == prev_version{
-                    //go to app
-                }else{
+                if version != prev_version {
                     //show a dialog pop up
                     let alert = TermsAlertView()
                     alert.show(animated: true)
                 }
             }else{
+                //for the first time
                 //show a dialog pop up
                 let alert = TermsAlertView()
                 alert.show(animated: true)
             }
         }
         
+        //make the OR button round
+        btn_or.layer.cornerRadius = 0.5 * btn_or.bounds.size.width
+        btn_or.clipsToBounds = true
+        
+        //change fb button text
+        let buttonText = NSAttributedString(string: "Continue With Facebook")
+        btn_fbLogin.setAttributedTitle(buttonText, for: .normal)
     
     }
 
